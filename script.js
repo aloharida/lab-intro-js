42dd93aa41e40e9839c10925753006cde952a85f
HARIDA <HARIDA@RIDA-MacBookPro-16.local>
2020-10-29 17:55:10 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//EXERCISE 1: The Fortune Teller
+let jobTitle = `Ux designer`
+let geoLocation = `toronto`
+let annualSalary= `90000`
+let companyName= `GREY`
+console.log(`You will be a ${jobTitle} in ${geoLocation}, making $${annualSalary} for ${companyName}.`)
