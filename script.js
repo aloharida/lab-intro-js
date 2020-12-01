2557632c5d8bc5324af8febf54d550069e5d42d2
HARIDA <HARIDA@RIDA-MacBookPro-16.local>
2020-12-01 18:16:29 +0900

    cccc

--- script.js
+++ script.js
@@ -1,177 +1,86 @@
 //EXERCISE 1: The Fortune Teller
+    //Why pay a fortune teller when you can just program your fortune yourself? Store the following into variables: job title, geographic location, annual salary, company name. Output your fortune to the console like so: "You will be a X in Y, making $N for Z."
 
-let jobTitle = `UX/UI designer`
-let geoLocation = `Newyork`
-let annualSalary = '99k'
-let companyName = `Grey`
+    let jobTitle = "UX Designer";
+    let geoLocation = "NEW YORK";
+    let companyName = "GRAY";
+    let annualSalary = "99M";
 
-console.log(`You will be a ${jobTitle} in ${geoLocation}, making $${annualSalary} for ${companyName}.`)
+   document.querySelector("#A-ex1").innerHTML =`You will be a ${jobTitle} in ${geoLocation}, making ${annualSalary} for ${companyName}.`;
 
-//EXERCISE 2: The Age Calculator
+   //EXERCISE 2: The Age Calculator
+    //Forgot how old someone is? Calculate it! Store the current year in a variable. Store their birth year in a variable. Calculate their age based on the stored values. Output them to the console like so: "They are NN years old.", substituting the values.
+    
+    let birthYear = 1994;
+    var date = new Date ( );
+    let currentYear = date.getFullYear( ) ;
+    
+    let  getAge = currentYear - birthYear;
 
-let currentYear = 2020;
-let birthYear = 1994;
-let age = currentYear - birthYear;
+    document.querySelector("#A-ex2").innerHTML = `You are ${
+      currentYear - birthYear
+    } years old.`;
 
-console.log(`They are ${age} years old.`);
+    //EXERCISE 3: The Lifetime Supply Calculator
+    //Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more! Store your current age into a variable. Store a maximum age into a variable. Store an estimated amount per day (as a number). Calculate how many you would eat total for the rest of your life. Output the result to the console like so: "You will need NN to last you until the ripe old age of X".
 
-//EXERCISE 3: The Lifetime Supply Calculator
+    let currentAge = 26; new Date().getFullYear() - birthYear;
+    let maxAge = 100;
+    let estAmount = 2;
 
+    document.querySelector("#A-ex3").innerHTML = `You will need ${
+      (maxAge - currentAge) * estAmount * 365
+    } pizza to last you until the ripe old age of ${maxAge}.`;
+  
+     //EXERCISE 4: The Geometrizer
+    //Calculate properties of a circle, using the definitions here. Store a radius into a variable. Calculate the circumference based on the radius, and output "The circumference is NN". Calculate the area based on the radius, and output "The area is NN".
+    let radius = 10;
+    let circumference = 2 * 3.14 * radius;
+    let area = (3.14 * radius) ** 2;
+    document.querySelector(
+      "#A-ex4"
+    ).innerHTML = `The circumference is ${circumference}. The area is ${area}.`;
+  
+    //EXERCISE 5: The Temperature Converter
+    //It's hot out! Let's make a converter based on the steps here. Store a celsius temperature into a variable. Convert it to fahrenheit and output "NN°C is NN°F".  Now store a fahrenheit temperature into a variable. Convert it to celsius and output "NN°F is NN°C."
+    let tempCel = 24;
+    let tempFar = 45;
+    document.querySelector("#A-ex5").innerHTML = `${tempCel}°C is ${
+      (tempCel * 9) / 5 + 24
+    }°F. ${tempFar}°F is ${((tempFar - 24) * 5) / 9}°C.`;
 
-let currentAge = 26;
-let maxAge = 99;
-let perDay = 100;
-let daysofYear = 365;
+    //FUNCTIONS 
+    // EXERCISE 1
+    // Write a function named squareNumber that will take one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+    function squareNumber(number) {
+      return number ** 4;
+    }
+    document.querySelector(
+      "#B-ex6"
+    ).innerHTML = `The result of squaring the number 4 is ${squareNumber(2)}.`;
+  
+    // EXERCISE 2
+    // Write a function named halfNumber that will take one argument (a number), divide it by 2, and return the result. It should also log a string like "Half of 5 is 2.5.".
+    function halfNumber(number) {
+      return number / 2;
+    }
+    document.querySelector("#B-ex7").innerHTML = `Half of 40 is ${halfNumber(
+      40
+    )}.`;
 
-let remainingYears = maxAge - currentAge;
-let dailySnacks = daysofYear * perDay;
-let totalSnacks = dailySnacks * remainingYears;
-
-console.log(`If I eat ${perDay} snacks a day, I'll need ${totalSnacks} snacks to last me until the ripe old age of ${maxAge}.`);
-
-//EXERCISE 4: The Geometrizer
-
-
-let radius = 6
-let pi = Math.PI
-let circumference = (2 * 5) * pi
-let area = pi * (radius ** 2)
-
-console.log(`The circumference is ${circumference}cm. The area is ${area}cm.`)
-
-//EXERCISE 5: The Temperature Converter
-
-
-let celciusTemp = 30;
-let fahrenheitTemp = celciusTemp * 9/5 + 32
-
-console.log(`${celciusTemp}°C is ${fahrenheitTemp}°F.`)
-
-fahrenheitTemp = 30;
-celciusTemp = (fahrenheitTemp - 32) * 5/9
-
-console.log(`${fahrenheitTemp}°F is ${celciusTemp}°C.`)
-
-
-// EXERCISE 1
-
-
-function squareNumber(num) {
-    console.log(`The square root of the number ${num} is: ${num ** 2}.`)
-    return num ** 2
-}
-
-let x = 6
-squareNumber(x)
-
-
-// EXERCISE 2
-// Write a function named halfNumber that will take one argument 
-//(a number), divide it by 2, and return the result. It should also 
-//log a string like "Half of 5 is 2.5.".
-
-// now using the console.log in function method, which i prefer
-//again so halfNumber(10) calls 10 into num, then do the calculations in that
-//function, and if you want to change the number, change it in halfResult
-function halfNumber(num) {
-    console.log(`Half of ${num} is ${num / 2}.`)
-    return num / 2
-}
-
-halfNumber(100)
-
-
-// EXERCISE 3
-// Write a function named percentOf that will take two numbers, 
-//figure out what percent the first number represents of the second number, 
-//and return the result. It should also log a string like "2 is 50% of 4."
-
-function percentOf(numA,numB){
-    let result;
-   
-    console.log(`${numA} is ${result = (numA / numB) * 100}% of ${numB}.`)
-    return result;
-}
-
-percentOf (10,5)
-
-
-// EXERCISE 4
-// Write a function named areaOfCircle that will take 
-//one argument (the radius), calculate the area based on that, and 
-//return the result. It should also log a string like 
-//"The area for a circle with radius 2 is 12.566370614359172."
-//Bonus: Round the result so there are only two digits after the decimal.
-
-function areaOfCircle (rad) {
-    let area = Math.PI * (rad ** 2)
-    console.log(`The area for a circle with radius ${rad} is ${area.toFixed(2)}.`)
-    return area
-}
-
-areaOfCircle(7)
-
-
-// EXERCISE 5
-// Write a function named runAll that will take one argument (a number) 
-// and perform the following operations, using the functions you wrote earlier
-//      1. Take half of the number and store the result.
-//      2. Square the result of #1 and store that result. 
-//      3. Calculate the area of a circle with the result of #2 as the radius.   
-//      4. Calculate what percentage that area (#3) is of the squared result (#2).
-
-
-function runAll (num) {
-
-    let half = halfNumber(num);
-    // this should take my half number function and half 7 into 3.5
-
-    let sqRoot = squareNumber(half);
-    // this should square root 3.5 cuz its taking my half var and using my function 
-    // squareNumber to square root it
-
-    let areaCir = areaOfCircle (sqRoot);
-    // this should calulate the area of 12.25, same concept as above
-
-    let perOf = percentOf(areaCir, sqRoot);
-    // this should tell me what percentage the area is of 12.25
-
-    return perOf
-}
-
-runAll(10)
-
-
-
-//DOM Exercise:
-
-let eleOne = document.querySelector(`#A-ex1`)
-eleOne.textContent = `You will be a ${jobTitle} in ${geoLocation}, making $${annualSalary} for ${companyName}.`
-
-let eleTwo = document.querySelector(`#A-ex2`)
-eleTwo.textContent = `They are ${age} years old.`
-
-let eleThree = document.querySelector(`#A-ex3`)
-eleThree.textContent = `If I eat ${perDay} snacks a day, I'll need ${totalSnacks} snacks to last me until the ripe old age of ${maxAge}.`
-
-let eleFour = document.querySelector(`#A-ex4`)
-eleFour.textContent = `The circumference is ${circumference}cm. The area is ${area}cm.`
-
-let eleFive = document.querySelector(`#A-ex5`)
-eleFive.textContent = `${fahrenheitTemp}°F is ${celciusTemp}°C and ${fahrenheitTemp}°F is ${celciusTemp}°C.`
-
-let eleSix = document.querySelector(`#B-ex6`)
-eleSix.textContent = `The square root of the number ${x} is ${squareNumber(6)}.`
-
-let eleSeven = document.querySelector(`#B-ex7`)
-eleSeven.textContent = `Half of 10 is ${halfNumber(100)}.`
-
-let eleEight = document.querySelector(`#B-ex8`)
-eleEight.textContent = `10 is ${percentOf(10,5)}% of 5.`
-
-let eleNine = document.querySelector(`#B-ex9`)
-eleNine.textContent = `The area for a circle with radius 7 is ${areaOfCircle(7).toFixed(2)}.`
-
-let eleTen= document.querySelector(`#B-ex10`)
-eleTen.textContent = `Half of 10 is ${halfNumber(10)}. The square root of the number 5 is ${squareNumber(5)}. The area for a circle with radius 25 is ${areaOfCircle(25).toFixed(2)}. 1963.50 is ${percentOf(1963,25)} % of 25.`
-
+     // EXERCISE 3
+    // Write a function named percentOf that will take two numbers, figure out what percent the first number represents of the second number, and return the result. It should also log a string like "2 is 50% of 4."
+    function percentOf(num1, num2) {
+      return (num1 / num2) * 100;
+    }
+    document.querySelector("#B-ex8").innerHTML = `5 is ${percentOf(
+      5,
+      10
+    )}% of 10.`;
+  // EXERCISE 4
+    // Write a function named areaOfCircle that will take one argument (the radius), calculate the area based on that, and return the result. It should also log a string like "The area for a circle with radius 2 is 12.566370614359172."
+    //      Bonus: Round the result so there are only two digits after the decimal.
+    function areaOfCircle(radius) {
+      return (radius * Math.PI * 2).toFixed(2);
+    }
+    document.querySelector("#B-ex9").innerHTML = `The area for a circle with radius 4 is ${areaOfCircle(4)}.`;
